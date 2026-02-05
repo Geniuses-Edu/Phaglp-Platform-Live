14873f3259db1a95ddec8f31e914ea5180efa344
Moe <72208447+mhamidawad@users.noreply.github.com>
2026-02-05 06:13:33 +0200

    Delete extracted directory

--- extracted/phaglp-platform28012026-master/__tests__/sample.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-
-describe('Sample Test', () => {
-  it('renders a simple test', () => {
-    render(<div>Hello Jest!</div>)
-    expect(screen.getByText('Hello Jest!')).toBeInTheDocument()
-  })
-})
